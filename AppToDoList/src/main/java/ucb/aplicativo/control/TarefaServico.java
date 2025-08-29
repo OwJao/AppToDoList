cc54727e8085415abd5a0caa97d3c5e3addbd0cd
OwJao <j.antonioassuncao@outlook.com>
2025-08-28 21:49:25 -0300

    Chore: Criação da estrutura inicial do projeto e dos três pacotes principais seguindo o modelo MVC.

---
+++ AppToDoList/src/main/java/ucb/aplicativo/control/TarefaServico.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucb.aplicativo.control;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author JOAO
+ */
+public class TarefaServico {
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private LocalDateTime horaAgora;
+    private boolean completa;
+}
