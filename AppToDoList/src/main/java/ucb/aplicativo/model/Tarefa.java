ac9f9da5931ced6993abdd521523aaeead9b9c00
Guilherme <guilhermeguedes2006@gmail.com>
2025-09-01 21:42:56 -0300

    feat: Adiciona classe Tarefa com construtores

--- AppToDoList/src/main/java/ucb/aplicativo/model/Tarefa.java
+++ AppToDoList/src/main/java/ucb/aplicativo/model/Tarefa.java
@@ -8,6 +8,52 @@ package ucb.aplicativo.model;
  *
  * @author JOAO
  */
+import java.time.LocalDateTime;
+
 public class Tarefa {
-    
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private LocalDateTime dataAgora;
+    private boolean completa;
+
+    public Tarefa() {
+        this.dataAgora = LocalDateTime.now();
+        this.completa = false;
+    }
+
+    public Tarefa(Long id, String titulo, String descricao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.completa = false;
+        this.dataAgora = LocalDateTime.now();
+    }
+
+    public Tarefa(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.completa = false;
+        this.dataAgora = LocalDateTime.now();
+    }
+
+    public Tarefa(Long id, String titulo, String descricao, boolean completa, LocalDateTime dataAgora) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.completa = completa;
+        this.dataAgora = dataAgora;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa {" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", completa=" + (completa ? "Sim" : "Não") +
+                ", dataCriacao=" + dataAgora +
+                '}';
+    }
 }
+
